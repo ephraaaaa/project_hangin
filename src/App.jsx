f3e644711133c2993115659383458c0fc36d190b
Ephraim John A. Gadin <ephraimjohn.gadin@gmail.com>
2024-07-01 12:16:11 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='flex justify-center items-center h-screen font-bold'>
+        Hello, world
+    </div>
+  )
+}
+
+export default App
