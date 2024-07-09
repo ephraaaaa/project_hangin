eec71593cf6a0731732b7fb79864c8b402df638a
Ephraim John A. Gadin <ephraimjohn.gadin@gmail.com>
2024-07-09 12:36:41 +0800

    Added Custom Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { useState } from 'react'
 import DisplayComponent from './DisplayComponent'
-import MapComponent from './MapComponent'
+import useFetchData from './useDataFetch';
 function App() {
-  const [count, setCount] = useState(0)
+  const { data, loading, error } = useFetchData('http://127.0.0.1:5000/api/data');
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
 
   return (
     <div className='flex justify-center items-center h-screen bg-slate-100 p-5'>
-        <MapComponent></MapComponent>
+      {data.slice(0, 3).map((locationData, index) => (
+        <DisplayComponent key={index} data={locationData} />
+      ))}
     </div>
-  )
+  );
 }
 
 export default App
+
+//import MapComponent from './MapComponent'
--- src/DisplayComponent.jsx
+++ src/DisplayComponent.jsx
@@ -1,79 +1,58 @@
-import React from 'react'
-import GaugeComponent from './GaugeComponent'
+import React from 'react';
+import GaugeComponent from './GaugeComponent';
 
-function DisplayComponent() {
-    return (
-        <div className='bg-white h-4/6 w-full flex flex-col rounded-xl shadow-lg p-4 m-3'>
-
-            {/* LOCATION */}
-
-            <div className='flex-1 flex m-1'>
-                <div className='flex flex-col m-2'>
-                    <div className='flex-1'></div>
-                    <div className='flex-1 flex flex-col justify-end'>
-                        <div className='bg-orange-500 text-white text-center rounded-full font-bold'>
-                            Medium
-                        </div> 
-                    </div>
-                    <div className='flex-1 flex flex-col'>
-                        <div className='flex-1 font-bold text-3xl text-wrap'>Arteche Blvd.</div>
-                        <div className='text-sm'>Catbalogan City, Samar - 6700</div>
-                    </div>
-                </div>
-                {/* GAUGE COMPONENT */}
-                <div className='flex-1 flex justify-center items-center rounded-lg'>
-                    <GaugeComponent value={75}/>
-                </div>
+function DisplayComponent({ data }) {
+  return (
+    <div className='bg-white h-4/6 w-full flex flex-col rounded-xl shadow-lg p-4 m-3'>
+      {/* LOCATION */}
+      <div className='flex-1 flex m-1'>
+        <div className='flex flex-col m-2'>
+          <div className='flex-1'></div>
+          <div className='flex-1 flex flex-col justify-end'>
+            <div className='bg-orange-500 text-white text-center rounded-full font-bold'>
+              {data.location.riskLevel}
             </div>
-
-            {/* PARTICULATE MATTER */}
-
-            <div className='flex-1 rounded-lg flex flex-col p-2'>
-
-                <div className='text-lg font-black'>
-                    PARTICULATE MATTER
-                </div>
-               
-                <div className='flex-1 flex font-bold'>
-
-                    {/* PM 1.0 */}
-
-                    <div className='flex-1 shadow-lg bg-white rounded-xl m-1 flex flex-col justify-center items-center p-2'>
-                        <div className='flex-1 flex justify-center items-center text-7xl'>25</div>
-                        <div className='bg-black rounded-full text-white text-center text-sm w-full'> μg/m³ (PM 1.0)</div>
-                    </div>
-
-                     {/* PM 2.5 */} 
-
-                    <div className='flex-1 shadow-lg bg-white rounded-xl m-1 flex flex-col justify-center items-center p-2'>
-                        <div className='flex-1 flex justify-center items-center text-7xl'>25</div>
-                        <div className='bg-black rounded-full text-white text-center text-sm w-full'> μg/m³ (PM 1.0)</div>
-                    </div>
-
-                     {/* PM 10.0 */}
-
-                    <div className='flex-1 shadow-lg bg-white rounded-xl m-1 flex flex-col justify-center items-center p-2'>
-                        <div className='flex-1 flex justify-center items-center text-7xl'>25</div>
-                        <div className='bg-black rounded-full text-white text-center text-sm w-full'> μg/m³ (PM 1.0)</div>
-                    </div>
-                </div>
-            </div>
-
-            {/* DUST */}
-
-             <div className='flex-1 rounded-lg flex flex-col p-3'>
-
-                <div className='text-lg font-black'>
-                    DUST CONCENTRATION
-                </div>
-               
-                <div className='bg-white shadow-lg rounded-lg flex-1'>
-                        
-                </div>
-            </div>
-
+          </div>
+          <div className='flex-1 flex flex-col'>
+            <div className='flex-1 font-bold text-3xl text-wrap'>{data.location.name}</div>
+            <div className='text-sm'>{data.location.address}</div>
+          </div>
         </div>
-    )
+        {/* GAUGE COMPONENT */}
+        <div className='flex-1 flex justify-center items-center rounded-lg'>
+          <GaugeComponent value={data.location.gaugeValue} />
+        </div>
+      </div>
+      {/* PARTICULATE MATTER */}
+      <div className='flex-1 rounded-lg flex flex-col p-2'>
+        <div className='text-lg font-black'>PARTICULATE MATTER</div>
+        <div className='flex-1 flex font-bold'>
+          {/* PM 1.0 */}
+          <div className='flex-1 shadow-lg bg-white rounded-xl m-1 flex flex-col justify-center items-center p-2'>
+            <div className='flex-1 flex justify-center items-center text-7xl'>{data.pm1}</div>
+            <div className='bg-black rounded-full text-white text-center text-sm w-full'> μg/m³ (PM 1.0)</div>
+          </div>
+          {/* PM 2.5 */}
+          <div className='flex-1 shadow-lg bg-white rounded-xl m-1 flex flex-col justify-center items-center p-2'>
+            <div className='flex-1 flex justify-center items-center text-7xl'>{data.pm2_5}</div>
+            <div className='bg-black rounded-full text-white text-center text-sm w-full'> μg/m³ (PM 2.5)</div>
+          </div>
+          {/* PM 10.0 */}
+          <div className='flex-1 shadow-lg bg-white rounded-xl m-1 flex flex-col justify-center items-center p-2'>
+            <div className='flex-1 flex justify-center items-center text-7xl'>{data.pm10}</div>
+            <div className='bg-black rounded-full text-white text-center text-sm w-full'> μg/m³ (PM 10.0)</div>
+          </div>
+        </div>
+      </div>
+      {/* DUST */}
+      <div className='flex-1 rounded-lg flex flex-col p-3'>
+        <div className='text-lg font-black'>DUST CONCENTRATION</div>
+        <div className='bg-white shadow-lg rounded-lg flex-1'>
+          {/* Dust data usage here */}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default DisplayComponent
+export default DisplayComponent;
